0da85c306cd7241ca7808b55ad184bc1db07ae99
Pankaj Nikumbh <nikumbhpankaj@170gmail.com>
2022-08-24 17:40:19 +0530

    Student class Updated

--- src/main/java/com/BikkadIT/model/Employee.java
+++ src/main/java/com/BikkadIT/model/Employee.java
@@ -2,4 +2,28 @@ package com.BikkadIT.model;
 
 public class Employee {
 
+	
+	private int Id;
+	
+	private String name;
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
 }
